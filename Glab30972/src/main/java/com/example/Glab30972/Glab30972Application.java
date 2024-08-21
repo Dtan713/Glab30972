07fea1a296ed9655cb3cb1b7eff747ad65ac006b
Dtan713 <s.donaldtan@gmail.com>
2024-08-21 10:18:10 -0400

    glab30972

---
+++ Glab30972/src/main/java/com/example/Glab30972/Glab30972Application.java
@@ -0,0 +1,13 @@
+package com.example.Glab30972;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Glab30972Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Glab30972Application.class, args);
+	}
+
+}
---
+++ Glab30972/src/test/java/com/example/Glab30972/Glab30972ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Glab30972;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Glab30972ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
